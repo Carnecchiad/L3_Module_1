b3937b6b4c7ee640cac6788c3bf9c9ee550fa7a5
katherine55 <league@WTS21.attlocal.net>
2017-10-01 12:52:35 -0700

    starts with m and rhymes with meth
    it’s meth

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package SearchingAndSortingAlgorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Algorithms {
@@ -10,47 +11,79 @@ public class Algorithms {
 			}
 
 		}
-		 return 0;
+		return 0;
 	}
+
 	public static int countPearls(List<Boolean> oysters) {
 		int amount = 0;
 		for (int i = 0; i < oysters.size(); i++) {
-			if(oysters.get(i) == true) {
+			if (oysters.get(i) == true) {
 				amount++;
 			}
 		}
 		return amount;
 	}
+
 	public static double findTallest(List<Double> peeps) {
 		double tallest = 0;
 		for (int i = 0; i < peeps.size(); i++) {
-			if(peeps.get(i) > tallest) {
+			if (peeps.get(i) > tallest) {
 				tallest = peeps.get(i);
 			}
 		}
 		return tallest;
 	}
+
 	public static String findLongestWord(List<String> words) {
 		int longest = 0;
 		for (int i = 0; i < words.size(); i++) {
-			if(words.get(i).length() > words.get(longest).length()) {
+			if (words.get(i).length() > words.get(longest).length()) {
 				longest = i;
 			}
 		}
 		return words.get(longest);
 	}
+
 	public static boolean containsSOS(List<String> message) {
 		for (int i = 0; i < message.size(); i++) {
-			if(message.get(i).contains("... --- ...")) {
+			if (message.get(i).contains("... --- ...")) {
 				return true;
 			}
 		}
 		return false;
+
+	}
+
+//	public static List<Double> sortScores(List<Double> results) {
+//		List<Double> y = new ArrayList<Double>();
+//		double smallest = 10000.0;
+//		double previousSmallest = 0.0;
+//		for (int i = 0; i < results.size(); i++) {
+//			for (int j = 0; j < results.size(); j++) {
+//				if(results.get(j) < smallest && results.get(j) > previousSmallest) {
+//					smallest = results.get(j);
+//					results.remove(j);
+//				}
+//			}
+//			y.add(smallest);
+//			previousSmallest = smallest;
+//			smallest = 0;
+//			
+//		}
+//		
+//		
+//
+//		return y;
+//	}
+	
+	public static List<String> sortDNA(List<String> unsortedSequences) {
+		List<String> newList = new ArrayList<String>();
 		
+		for (int i = 0; i < unsortedSequences.size(); i++) {
+			
+		}
+		return newList;
 	}
-	public static double sortScores(List<Double> results) {
-		
 	}
-	}
-	// Add other methods here
 }
+// Add other methods here
